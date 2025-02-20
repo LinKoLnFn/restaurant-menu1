d08e84d4c41bc99634f4cb8c9ed74e60156f314c
LinKoLnFn <linkolntv123@gmail.com>
2025-02-20 14:01:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-function showCategory(category) {
-    document.querySelectorAll('.menu-category').forEach(section => {
-        section.classList.remove('active');
+function openCategory(category) {
+    document.querySelectorAll('.menu-category').forEach(item => {
+        item.style.display = "none";
     });
-    document.getElementById(category).classList.add('active');
+    document.getElementById(category).style.display = "block";
 }
 
 function toggleDescription(item) {
-    document.querySelectorAll('.description').forEach(desc => {
-        desc.style.display = 'none';
-    });
-
-    let desc = item.querySelector('.description');
+    let desc = document.querySelector(`#${item} .item-desc`);
     if (desc) {
-        desc.style.display = 'block';
+        let allDescriptions = document.querySelectorAll('.item-desc');
+        allDescriptions.forEach(d => d.style.display = "none");
+        desc.style.display = desc.style.display === "block" ? "none" : "block";
     }
 }
